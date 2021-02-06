3099684771c9e4dc98c936d35f00127549efe02f
Necroxos <rdoratm@outlook.com>
2021-02-06 12:06:27 -0300

    Cambiando env y agregando Docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'http://127.0.0.1:3000',
+  API_URL: 'http://192.168.0.11:99',
   mapboxKey: 'pk.eyJ1IjoiY2V2aW4yMDIxIiwiYSI6ImNra212MXlpZjAzaTgybnA1ZHpyaTgwMGMifQ.EbD0YvUk899q-_fqwWbIKQ'
 };
